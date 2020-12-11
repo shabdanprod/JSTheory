476393cf5de7be38121c96d938f3ec26647858f1
Shabdan <shabdan.prod@gmail.com>
2020-12-11 17:41:43 +0600

    Added some comments

--- Tsykly 014/js/script.js
+++ Tsykly 014/js/script.js
@@ -2,14 +2,14 @@
 
 let num = 50;
 
-while (num <= 55) {
+while (num <= 55) { //Cycle While
     console.log(num);
     num++;
 }
 
 
 let num = 50;
-do {
+do { ////Cycle Do While
     console.log(num);
     num++;
 }
@@ -17,7 +17,7 @@ while(num < 55);
 
 let num = 50;
 
-for (let i = 1; i < 8; i++) {
+for (let i = 1; i < 8; i++) { // //Cycle for
     console.log(i);
 }
 
