9718dc77c6f4598241874ab1c2238d1ad2000279
Shabdan <shabdan.prod@gmail.com>
2020-12-11 16:17:41 +0600

    Uslovie Switch Сравнительное

--- js/script.js
+++ js/script.js
@@ -23,3 +23,23 @@ if (num < 49) {
 }
 
 (num === 50) ? console.log('ok!') : console.log('Error');
+
+//
+
+const num = 50;
+
+switch (num) {
+    case 49: 
+        console.log('Неверно!')
+    break;
+    case 100: 
+        console.log('Неверно!')
+    break;
+    case 50: 
+        console.log('В Точку!')
+    break;
+    default:
+        console.log('Не в этот Раз');
+        break;
+}
+
