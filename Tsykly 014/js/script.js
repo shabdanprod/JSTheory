3dd9f98d3ec2ae7e87bbb0819a4e97617c3f21a3
Shabdan <shabdan.prod@gmail.com>
2020-12-11 17:28:28 +0600

    3rd Cycle For

--- Tsykly 014/js/script.js
+++ Tsykly 014/js/script.js
@@ -1,3 +1,29 @@
 "use strict";
 
-const num = 50;
+let num = 50;
+
+while (num <= 55) {
+    console.log(num);
+    num++;
+}
+
+
+let num = 50;
+do {
+    console.log(num);
+    num++;
+}
+while(num < 55);
+
+let num = 50;
+
+for (let i = 1; i < 8; i++) {
+    console.log(i);
+}
+
+let num = 50;
+
+for (let i = 1; i < 8; i++) {
+    console.log(num);
+    num++;
+}
